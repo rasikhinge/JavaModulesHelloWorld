28f42a40dd420123af0101ad487e80ea3dcabac6
rasikhinge <rasik.hinge4@gmail.com>
2020-07-05 15:27:57 +0530

    ServiceLoader changes

--- org.modules.jokeserver.kids/src/module-info.java
+++ org.modules.jokeserver.kids/src/module-info.java
@@ -1,6 +1,6 @@
 module org.modules.jokeserver.kids {
     requires org.modules.jokeserver;
-    exports org.modules.jokeserver.kids;
+//    exports org.modules.jokeserver.kids;
 
     provides org.modules.jokeserver.JokeServer
             with org.modules.jokeserver.kids.KidsJokeServer;
--- org.modules.jokeserver.programmers/src/module-info.java
+++ org.modules.jokeserver.programmers/src/module-info.java
@@ -1,6 +1,6 @@
 module org.modules.jokeserver.programmers {
     requires org.modules.jokeserver;
-    exports org.modules.jokeserver.programmers;
+//    exports org.modules.jokeserver.programmers;
 
     provides org.modules.jokeserver.JokeServer
             with org.modules.jokeserver.programmers.ProgrammersJokeServer;
